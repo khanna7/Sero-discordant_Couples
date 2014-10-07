9bf8c13cd22f4535ecb8d3ec8d88fbadac30b8b5
Aditya Khanna <khanna7@uw.edu>
2014-10-07 14:11:31 -0700

    Clean up. Add coverage parameter

--- Code/Scratchpad/identify_couples_d1.R
+++ Code/Scratchpad/identify_couples_d1.R
@@ -39,9 +39,6 @@
       load("../../Burnin_Data/burnin.21Feb_UG_phi004_newatts_run1.RData")
       ug.nw <- network.extract(nw, at=1041)
 
-      load("../../Burnin_Data/burnin.12March_ZA_phi004_incdur1000_newatts_30yrburn_run1.RData")
-      za.nw <- network.extract(nw, at=781)
-
 ##############################################################
 
 ##############################################################
@@ -56,24 +53,16 @@
          ## nw.el <- as.edgelist(nw)
 
       ## Relevant attributes of actors
-         status.el <- matrix((nw %v% "inf.status")[nw.el], ncol = 2)
-         inf.time <- nw %v% "inf.time"
-         time.since.infection <- nw %v% "time.since.infection" # ASK
-
+         status.el <- matrix((nw %v% "inf.status")[nw.el],
+                             ncol = 2)
          inf.status <- nw %v% "inf.status"
-         circum.status <- nw %v% "circum.status" # ASK
-         curr.pregnancy.status <- nw %v% "curr.pregnancy.status" # ASK
-         art.status <- nw %v% "art.status" # ASK
-         infectivity.today <- nw %v% "infectivity.today" # ASK
-         age <- nw%v%"age" #22Aug2013
-         sex <- nw%v%"sex" ## 30 Oct 2013 -- more attribute inf recorded
-         cd4.count.today <- nw%v%"cd4.count.today" ## for understanding detail about infectors
-         viral.load.today <- nw%v%"viral.load.today"
-         preg.status <- nw%v%"curr.pregnancy.status"
-         circum.status <- nw%v%"circum.status"
-         time.since.infection <- nw%v%"time.since.infection"
          art.status <- nw%v%"art.status"
-         art.type <- nw%v%"art.type"
+
+      ## Relevant attributes of edges
+         primary.sdp <- nw%e%"primary.sdp"
+
+      ## Parameter for SDP coverage
+         sdp.coverage <- 0.80
 
       ## Male partner HIV+
          discordant.mpos <- intersect(which(status.el[, 1] == 1),
@@ -89,28 +78,6 @@
          transmittable.f <- nw.el[discordant.fpos, 2]
          infectible.m    <- nw.el[discordant.fpos, 1]
 
-      ## hiv-infected serodiscordant men with multiple partners
-         sdp.m.conc.p <- transmittable.m[which(duplicated(transmittable.m))]
-  
-      ## hiv-infected serodiscordant women with multiple partners
-         sdp.f.conc.p <- transmittable.f[which(duplicated(transmittable.f))]
-
-      ## we now know which:
-         ##1. hiv-infected serodiscordant men
-         ##   have multiple partners, and,
-         ##2. hiv-infected serodiscordant women
-         ##   have multiple partners, and,
-
-      ## find edgeIDs for multiple partnerships of
-      ## HIV-infected men
-         which((nw.el[,1]) == sdp.m.conc.p[1]) ###...
-         which((nw.el[,1]) == sdp.m.conc.p[length(sdp.m.conc.p)])
-
-      ## find edgeIDs for multiple partnerships of
-      ## HIV-infected women
-         which((nw.el[,2]) == sdp.f.conc.p[1]) ###...
-         which((nw.el[,2]) == sdp.f.conc.p[length(sdp.f.conc.p)])
-
       ## now obtain formation, dissolution and
       ## duration from "egde.activity," 
          nw.el.activity <- unlist(get.edge.activity(nw))
@@ -123,7 +90,7 @@
          
       ## for longest partnership of HIV-infected partners
       ## set "primary.sdp"=1
-
+         sdp.coverage <- 1
          primary.sdp <- rep(-1, nrow(nw.el))
 
          ## where infected partner is male
@@ -139,9 +106,13 @@
                )
            primary.sdp[edgeID[sht.pt]] <- 1
            ## update ART status
+           coin.toss <- runif(1, 0, 1)
+           cat("Coin-toss for men is ", coin.toss, "\n")
+           if (coin.toss <= sdp.coverage){
            art.status[nw.el[edgeID[sht.pt], 1]] <- 1
            cat("ART-statuses to be updated are ",
                (nw.el[edgeID[sht.pt], 1]), "\n")
+         }
          } 
 
          ## where infected partner is female
@@ -155,13 +126,24 @@
                )
            primary.sdp[edgeID[sht.pt]] <- 1
            ## update ART status
+           coin.toss <- runif(1, 0, 1)
+           cat("Coin-toss for women is ", coin.toss, "\n")
+           if (coin.toss <= sdp.coverage){
            art.status[nw.el[edgeID[sht.pt], 2]] <- 1
            cat("ART-statuses to be updated are ",
                (nw.el[edgeID[sht.pt], 2]), "\n")
+         }
          } 
 
-                 
+         ## to check, following code can be used
+            ## Set sdp.coverage=1
+            ## (nw%v%"sex")[11093] #sex of +ve partner
+            ## (nw%v%"inf.status")[11093] #infection status
+            ## (nw%v%"art.status")[11093] #old ART status
+            ## (art.status)[11093] #updated ART status
+      
          nw%v%"art.status" <- art.status
+         nw%e%"primary.sdp" <- primary.sdp
 
 ##############################################################
 
