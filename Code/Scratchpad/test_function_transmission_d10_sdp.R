15495bed8ce44b89f30f295271ffedd001c40eef
Aditya Khanna <khanna7@uw.edu>
2014-10-17 13:18:47 -0700

    unit test for transmission : lost and recreated

--- Code/Scratchpad/test_function_transmission_d10_sdp.R
+++ Code/Scratchpad/test_function_transmission_d10_sdp.R
@@ -21,24 +21,53 @@
 ##############################################################
    ## Read in burnin data
       load("../../Burnin_Data/burnin.21Feb_UG_phi004_newatts_run1.RData")
-      ## ug.nw <- network.extract(nw, at=1041)
 
    ## Source function
-      source("function_identify_couples_d1.R")
+      source("transmission_d10_sdp.R")
+      ##source("transmission_d10.R")
+##############################################################
+
+##############################################################
+   ## Needed information
+      ## parameters
+         nw <- network.extract(nw, at=1042)
+         verbose <- TRUE
+         preg.susc.mult <- 1
+         circum.mult <- 1
+         scenario <- "baseline" 
+         baseline.art.coverage.rate <- 1
+         baseline.preg.coverage.rate <- 1
+         given.dur.inf.by.age <- 100 
+         eligible.cd4 <- 1000 
+         baseline.f.ges.visit <- 1 
+         decline.ui <- 1
+
+      ## edge attributes
+         set.edge.attribute(nw, "known.sdp", 1)
 ##############################################################
 
 ##############################################################
 
    ## Test
       ## Simulate
-         set.seed(7)
-         test.net <- identify.sdp.update.art(nw=nw,
-                                             verbose=TRUE,
-                                             sdp.coverage=1,
-                                             time=1042
-                                             )
+         set.seed(1234)
+         test.net <- transmission(nw=nw,
+                                  verbose=verbose,
+                                  preg.susc.mult=preg.susc.mult,
+                                  circum.mult=circum.mult, 
+                                  scenario=scenario,
+                                  baseline.art.coverage.rate=
+                                  baseline.art.coverage.rate,
+                                  baseline.preg.coverage.rate=
+                                  baseline.preg.coverage.rate,
+                                  given.dur.inf.by.age=
+                                  given.dur.inf.by.age,
+                                  eligible.cd4=
+                                  eligible.cd4,
+                                  baseline.f.ges.visit=
+                                  baseline.f.ges.visit,
+                                  decline.ui=
+                                  decline.ui
+                                  )
 
-      ## Compare results
-         (nw%v%"art.status")[24148] # before sdp intervention
-         (test.net%v%"art.status")[24148] # after sdp intervenion
 ##############################################################
