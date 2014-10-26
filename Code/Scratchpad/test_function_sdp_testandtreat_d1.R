847f1b8875e28ab5550848ca126d968094876267
Aditya Khanna <khanna7@uw.edu>
2014-10-26 16:38:25 -0700

    bug in test and treat. add functionality for all

--- Code/Scratchpad/test_function_sdp_testandtreat_d1.R
+++ Code/Scratchpad/test_function_sdp_testandtreat_d1.R
@@ -5,6 +5,9 @@
 ##############################################################
 ### Progression of versions
 ##############################################################
+
+### 26 Oct 2014: Add testing of all on a/c of HBHTC
+
 ### 17 Oct 2014: Test wrapper function
 ##############################################################
 
@@ -28,12 +31,17 @@
 
 ##############################################################
    ## Needed parameters
-      ##nw = network.extract(nw, retain.all.vertices=TRUE)
+      nw = network.extract(nw, at=1042)
       verbose = TRUE
+
       sdp.testing.coverage = 1
       sdp.art.at.cd4 = 350
       sdp.art.coverage = 1
       time=1041
+
+      hbhtc.all.testing.coverage=0.80
+      hbhtc.all.art.at.cd4=1000
+      hbhtc.all.art.coverage=0.764
 
    ## Needed edge attributes
       set.edge.attribute(nw, "primary.sdp", 1)
@@ -48,7 +56,13 @@
                                    sdp.art.at.cd4,
                                    sdp.art.coverage=
                                    sdp.art.coverage,
-                                   time=time
+                                   time=time,
+                                   hbhtc.all.testing.coverage=
+                                   hbhtc.all.testing.coverage,
+                                   hbhtc.all.art.at.cd4=
+                                   hbhtc.all.art.at.cd4,
+                                   hbhtc.all.art.coverage=
+                                   hbhtc.all.art.coverage
                                    )
-
+      nw%v%"art.status"
 ##############################################################
