80275b8986686ec21936d5492f0ec7130af79526
Aditya Khanna <khanna7@uw.edu>
2014-10-26 18:01:32 -0700

    Finished debugging test and treat

--- Code/Engine/function_sdp_testandtreat_d1.R
+++ Code/Engine/function_sdp_testandtreat_d1.R
@@ -6,10 +6,12 @@
 ##############################################################
 ### Progression of versions
 ##############################################################
-### 26 Oct 2014: We should not be extracting only cross-sectional network
+### 26 Oct 2014:
+    ## a. We should not be extracting only cross-sectional network
     ## at the top and calling it "nw." That changes the network object
-    ## "nw" that all processes are run on. Comment out extraction of
-    ## cross-sectional network at the top
+    ## "nw" that all processes are run on. Only edgelist is needed.
+
+    ## b. Test for all via HBHTC: then treat for known-SDP & not-known-SDP
 
 ### 17 Oct 2014: Wrapper function
 
@@ -28,21 +30,16 @@
   ## Function
      testandtreat.sdp <- function(nw,
                                   verbose,
-                                  sdp.testing.coverage,
-                                  sdp.art.at.cd4,
-                                  sdp.art.coverage,
+                                  hbhtc.testing.coverage,
+                                  known.sdp.art.coverage,
+                                  notknown.sdp.art.coverage,
+                                  known.sdp.art.at.cd4,
+                                  notknown.sdp.art.at.cd4,
                                   time,
-                                  hbhtc.all.testing.coverage,
-                                  hbhtc.all.art.at.cd4,
-                                  hbhtc.all.art.coverage,
                                   ...
                                   ){
        
 
-  ## Extract cross-sectional network
-     ## nw <- network.extract(nw, at=time,
-     ##                       retain.all.vertices=TRUE)
-     
   ## Extract relevant node attributes
      inf.status <- nw%v%"inf.status"
      cd4.count.today <- nw%v%"cd4.count.today"
@@ -58,58 +55,49 @@
      status.el <- matrix((nw %v% "inf.status")[nw.el],
                           ncol = 2)
 
- ############# TEST AND TREAT FOR ALL VIA HBHTC########################
-
+ ###########################################################
+ ############# TEST FOR ALL VIA HBHTC
+ ###########################################################
+     
   ## Identify population size
      pop.size <- network.size(nw)
      
   ## testing all
-     tested.and.artinit.today <- rbinom(n=pop.size, size=1,
-                                        prob=hbhtc.all.testing.coverage*hbhtc.all.art.coverage)
+     tested.today <- rbinom(n=pop.size, size=1,
+                            prob=hbhtc.testing.coverage)
 
-  ## Treatment all    
-     tested.and.artinit.today.untreated <- intersect(which(tested.and.artinit.today == 1), which(art.status == 0))
-        ##art.status=0 is only for those with inf.status=1. For those who have inf.status=0,
-        ##art.status is NA.
-     art.status[tested.and.artinit.today.untreated] <- 1 #update attribute
-     art.type[tested.and.artinit.today.untreated]   <- 1 #update attribute
-
-     ## browser()
+ ###########################################################
+  ############# TREAT  KNOWN-SDP VIA HBHTC
+ ###########################################################
      
-  ############# TEST and TREAT FOR SDP VIA HBHTC########################
   ## identify couples eligible for test-and-treat
      partner.cum.status <- rowSums(status.el)
      primary.sdp.check <- intersect(which(partner.cum.status == 1),
                                     which(primary.sdp == 1))
-     ## primary.sdp.check <- intersect(primary.sdp.check,
-     ##                                which(known.sdp != 1))
-                                      #if a couple is already "known SDP",
-                                      #then test&treat routine does not apply
 
   ## test-sdp 
      test.el <- matrix(0,
-                       nrow=nrow(status.el),
-                       ncol=ncol(status.el))
+                       nrow=nrow(nw.el),
+                       ncol=ncol(nw.el))
 
-     for(i in 1:length(primary.sdp.check)){
-       couple <- primary.sdp.check[i]
-       test.el[couple, 1] <- rbinom(1, 1, #first ptn in sdp
-                                    sdp.testing.coverage)
-       test.el[couple, 2] <- rbinom(1, 1, #sec ptn in sdp
-                                    sdp.testing.coverage)
-       
+     ## browser()
+     for(i in 1:nrow(nw.el)){
+       test.el[i, 1] <- tested.today[nw.el[i, 1]]
+       test.el[i, 2] <- tested.today[nw.el[i, 2]]
      }
 
-     known.sdp <- which(rowSums(test.el) == 2)
+     known.sdp <- intersect(which(rowSums(status.el) == 1),
+                            which(rowSums(test.el) == 2))
 
 
-   ## treat-sdp
+   ## treat known-sdp
+     if(length(known.sdp) > 0){
        for (i in 1:length(known.sdp)){
          pos.partner <- which(status.el[known.sdp[i],] == 1)
          pos.ind <- nw.el[known.sdp[i],pos.partner]
-         if (cd4.count.today[pos.ind] <= sdp.art.at.cd4){
+         if (cd4.count.today[pos.ind] <= known.sdp.art.at.cd4){
            if (art.status[pos.ind] != 1){
-             coin.flip <- rbinom(1, 1, sdp.art.coverage)
+             coin.flip <- rbinom(1, 1, known.sdp.art.coverage)
              art.status[pos.ind] <- coin.flip
              if (coin.flip == 1){
                art.type[pos.ind] <- 1
@@ -117,7 +105,33 @@
            }
          }
        }
+     }
 
+###########################################################
+  ############# TREAT  NOT-KNOWN-SDP VIA HBHTC
+ ###########################################################
+   ## browser()
+     
+   notknown.sdp <- intersect(which(rowSums(status.el) == 1),
+                               which(rowSums(test.el) == 1))
+     
+    ## treat not-known-sdp
+       if (length(notknown.sdp) > 0){
+       for (i in 1:length(notknown.sdp)){
+         pos.partner <- which(status.el[notknown.sdp[i],] == 1)
+         pos.ind <- nw.el[notknown.sdp[i],pos.partner]
+         if (cd4.count.today[pos.ind] <= notknown.sdp.art.at.cd4){
+           if (art.status[pos.ind] != 1){
+             coin.flip <- rbinom(1, 1, notknown.sdp.art.coverage)
+             art.status[pos.ind] <- coin.flip
+             if (coin.flip == 1){
+               art.type[pos.ind] <- 1
+             } 
+           }
+         }
+       }
+     }
+     
   ## Update vertex attributes
      nw%v%"art.status" <- art.status #16oct14
      nw%v%"art.type" <- art.type #16oct14
