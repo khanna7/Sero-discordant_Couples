070f331307731ccdbd02d8e011ec3b50c479a3f5
Aditya Khanna <khanna7@uw.edu>
2015-03-01 14:56:04 -0800

    add plot files

--- Code/Uganda_Analysis_Do_Not_Commit/plots_sdp_interv.R
+++ Code/Uganda_Analysis_Do_Not_Commit/plots_sdp_interv.R
@@ -5,6 +5,8 @@
 ##########################################
 ### Progression of versions
 ##########################################
+## 1 Mar 2015: combine scenario IV results here
+
 ## 7 Nov 2014: plot runs for uganda
 ##########################################
 
@@ -18,6 +20,16 @@ rm(list=ls())
 ##########################################
 ### Read data from CSV files
 ##########################################
+
+ ## Baseline
+   for (i in 1:n.sim){
+     assign(paste("ug.bl.cp.run.",i, sep=""),
+            read.csv(paste("~/Projects/Home-Based/PMTCT/Code/Development2/Uganda_Runs/28Feb_UG_bl_cp_run", i,
+                           ".prev.inc.data.csv",
+                           sep=""),
+                     sep=" ", header=FALSE)
+            )
+   }                    
 
   ## SDP current coverage
      for (i in 1:n.sim){
@@ -39,9 +51,26 @@ rm(list=ls())
               )
      }                    
 
+  ## SDP high coverage
+     for (i in 1:n.sim){
+       assign(paste("ug.sdp.scenarioIV.run.",i, sep=""),
+              read.csv(paste("../Uganda_Runs_Do_Not_Commit/28Feb_UG_sdp_scenarioIV_run", i,
+                             ".prev.inc.data.csv",
+                             sep=""),
+                       sep=" ", header=FALSE)
+              )
+     }                    
+
 ##########################################
 ### Compute mean across ten years
 ##########################################
+
+  ## Baseline
+     mean.inci.bl.cp <- matrix(NA, ncol=n.sim, nrow=260)
+     for (i in 1:n.sim){ 
+       data <- get(paste("ug.bl.cp.run.",i, sep=""))
+       mean.inci.bl.cp[,i] <- (data[,6]/(data[,2]-data[,7])) 
+     }
 
   ## SDP curr
      mean.inci.sdp.curr <- matrix(NA, ncol=n.sim, nrow=260)
@@ -57,6 +86,13 @@ rm(list=ls())
        mean.inci.sdp.high[,i] <- (data[,6]/(data[,2]-data[,7])) 
      }
 
+  ## SDP scenario IV model testing
+     mean.inci.sdp.scenarioIV <- matrix(NA, ncol=n.sim, nrow=260)
+     for (i in 1:n.sim){ 
+       data <- get(paste("ug.sdp.scenarioIV.run.",i, sep=""))
+       mean.inci.sdp.scenarioIV[,i] <- (data[,6]/(data[,2]-data[,7])) 
+     }
+
 ##########################################
 
 ##########################################
@@ -67,6 +103,16 @@ rm(list=ls())
   ## mean for each year over 10 years +/-
   ## t*sd(for each year over 10 years)/sqrt(n)
   ## where n is the number of simulations
+
+     ## Baseline
+     bl.cp <- apply(mean.inci.bl.cp, 1, mean)
+
+     bl.cp <- split(bl.cp, ceiling(seq_along(bl.cp)/26))
+     bl.cp.mean <- unlist(lapply(bl.cp, mean))*26*100
+     bl.cp.lowci <- (bl.cp.mean)-((qt(0.975, df=n.sim-1)*unlist(lapply(bl.cp,
+                                                     sd))*(26*100))/sqrt(n.sim)) 
+     bl.cp.upci <- (bl.cp.mean)+((qt(0.975, df=n.sim-1)*unlist(lapply(bl.cp,
+                      sd))*(26*100))/sqrt(n.sim)) ##30Jun14
 
      ## SDP current
      sdp.curr <- apply(mean.inci.sdp.curr, 1, mean)
@@ -89,14 +135,36 @@ rm(list=ls())
                       sd))*(26*100))/sqrt(n.sim)) ##30Jun14
 
 
-     ug.inc.data <- cbind(sdp.curr.mean, sdp.curr.lowci, sdp.curr.upci,
-                          sdp.high.mean, sdp.high.lowci, sdp.high.upci)
+     ug.inc.data <- cbind(
+                      bl.cp.mean, bl.cp.lowci, bl.cp.upci,
+                      sdp.curr.mean, sdp.curr.lowci, sdp.curr.upci,
+                      sdp.high.mean, sdp.high.lowci, sdp.high.upci)
+
+     ## Scenario IV model testing
+     sdp.scenarioIV <- apply(mean.inci.sdp.scenarioIV, 1, mean)
+
+     sdp.scenarioIV <- split(sdp.scenarioIV, ceiling(seq_along(sdp.scenarioIV)/26))
+     sdp.scenarioIV.mean <- unlist(lapply(sdp.scenarioIV, mean))*26*100
+     sdp.scenarioIV.lowci <- (sdp.scenarioIV.mean)-((qt(0.975, df=n.sim-1)*unlist(lapply(sdp.scenarioIV,
+                                                     sd))*(26*100))/sqrt(n.sim)) 
+     sdp.scenarioIV.upci <- (sdp.scenarioIV.mean)+((qt(0.975, df=n.sim-1)*unlist(lapply(sdp.scenarioIV,
+                      sd))*(26*100))/sqrt(n.sim)) ##30Jun14
+
+
+     ug.inc.data <- cbind(
+                      bl.cp.mean, bl.cp.lowci, bl.cp.upci,
+                      sdp.curr.mean, sdp.curr.lowci, sdp.curr.upci,
+                      sdp.high.mean, sdp.high.lowci, sdp.high.upci,
+                      sdp.scenarioIV.mean, sdp.scenarioIV.lowci, sdp.scenarioIV.upci
+                      )
 
 
 
      colnames(ug.inc.data) <-
-       c("SDP.Curr.Mean", "SDP.Curr.LowCI", "SDP.Curr.HighCI",
-         "SDP.High.Mean", "SDP.High.LowCI", "SDP.High.HighCI")
+       c("Baseline.Curr.Mean", "Baseline.Curr.LowCI", "Baseline.Curr.HighCI",
+         "SDP.Curr.Mean", "SDP.Curr.LowCI", "SDP.Curr.HighCI",
+         "SDP.High.Mean", "SDP.High.LowCI", "SDP.High.HighCI",
+         "SDP.ScenarioIV.Mean", "SDP.ScenarioIV.LowCI", "SDP.ScenarioIV.HighCI")
 
 ##########################################
 
